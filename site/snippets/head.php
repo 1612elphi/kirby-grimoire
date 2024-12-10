2578dd3dd738baf40cd49bbd50ab3af4a283ba8e
Ruby Morgan Voigt <ruby@rmv.fyi>
2024-12-10 12:09:40 -0600

    fixed full screen behaviour

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -90,14 +90,26 @@
     <?php endforeach ?>
     
     <script>
-        function toggleFullScreen() {
-            if (!document.fullscreenElement) {
-                document.documentElement.requestFullscreen();
-            } else {
-                if (document.exitFullscreen) {
-                    document.exitFullscreen();
-                }
-            }
-        }
+    // Function to toggle fullscreen
+    function toggleFullScreen() {
+      if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen().then(() => {
+          localStorage.setItem('isFullscreen', 'true');
+        });
+      } else {
+        document.exitFullscreen().then(() => {
+          localStorage.setItem('isFullscreen', 'false');
+        });
+      }
+    }
+    
+    // Check and restore fullscreen state on page load
+    document.addEventListener('DOMContentLoaded', () => {
+      if (localStorage.getItem('isFullscreen') === 'true') {
+        document.documentElement.requestFullscreen().catch(err => {
+          console.log('Error attempting to enable fullscreen:', err);
+        });
+      }
+    });
     </script>
 </head>
