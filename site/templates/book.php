ef5077dc4ef9977712b626529f22c77bdff5cf35
Ruby Morgan Voigt <ruby@rmv.fyi>
2024-12-10 12:20:06 -0600

    disabling full screen button while issue 1 gets fixed

--- site/templates/book.php
+++ site/templates/book.php
@@ -17,11 +17,11 @@
     </svg>
   </a>
 
-  <button onclick="toggleFullScreen()" class="flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-gray-100 rounded-md hover:bg-gray-200 transition-colors">
+  <!-- <button onclick="toggleFullScreen()" class="flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-gray-100 rounded-md hover:bg-gray-200 transition-colors">
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor">
       <path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"/>
     </svg>
-  </button>
+  </button> -->
 </nav>
 
 <div class="flex mx-8">
